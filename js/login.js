a3d6189a04429a782653cb37e18c7987eb4bbec4
Vinicius Nascimento <viniciusncrocha@gmail.com>
2023-05-02 02:46:06 -0300

    tela de login e mudanças menores

---
+++ js/login.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+	tab = $('.tabs h3 a');
+
+	tab.on('click', function(event) {
+		event.preventDefault();
+		tab.removeClass('active');
+		$(this).addClass('active');
+
+		tab_content = $(this).attr('href');
+		$('div[id$="tab-content"]').removeClass('active');
+		$(tab_content).addClass('active');
+	});
+});
+
+const loginButton = document.getElementById("#login_button");
+
+loginButton.addEventListener("click", function() { 
+    window.location.href = "../html/agendamento.html";  
+});
